24fb1dc56cd6227fbe5973332923ae3307ab0cbe
Mariano Van Staden <106852546+MarianoVanStaden@users.noreply.github.com>
2024-09-13 00:45:11 -0300

    Update app.component.ts

--- bilog-2/src/app/app.component.ts
+++ bilog-2/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Bilog-2 - Light weight baby';
+  title = 'Bilog-2';
 }
